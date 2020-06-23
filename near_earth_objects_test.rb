7da5069a38020e77395374af314f00fd167b753e
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-23 12:46:03 -0600

    Update runner file and remove old redundant code

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -9,11 +9,6 @@ class NearEarthObjectsTest < Minitest::Test
     @results = NearEarthObjects.new('2019-03-30')
   end
 
-  # def test_a_date_returns_a_list_of_neos
-  #   results = NearEarthObjects.new('2019-03-30')
-  #   assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
-  # end
-
   def test_it_exists
     assert_instance_of NearEarthObjects, @results
   end
