4fb0a67fb98c2b1b44eef8611536a5cd93cdedd3
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-23 10:32:35 -0600

    Add largest astroid diameter method and test

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,11 +1,13 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
+require 'JSON'
 # Load ENV vars via Figaro
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
 Figaro.load
 
 class NearEarthObjects
+  attr_reader :parsed_asteroids_data
   def self.find_neos_by_date(date)
     conn = Faraday.new(
       url: 'https://api.nasa.gov',
@@ -33,5 +35,22 @@ class NearEarthObjects
       biggest_astroid: largest_astroid_diameter,
       total_number_of_astroids: total_number_of_astroids
     }
+  end
+
+  def initialize(date)
+    conn = Faraday.new(
+      url: 'https://api.nasa.gov',
+      params: { start_date: date, api_key: ENV['nasa_api_key']}
+    )
+    asteroids_list_data = conn.get('/neo/rest/v1/feed')
+
+    @parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  end
+
+  def largest_astroid_diameter
+    largest_astroid = parsed_asteroids_data.max_by do |astroid|
+      astroid[:estimated_diameter][:feet][:estimated_diameter_max]
+    end
+    largest_astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
   end
 end
--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -4,8 +4,21 @@ require 'pry'
 require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
-  def test_a_date_returns_a_list_of_neos
-    results = NearEarthObjects.find_neos_by_date('2019-03-30')
-    assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
+
+  def setup
+    @results = NearEarthObjects.new('2019-03-30')
+  end
+
+  # def test_a_date_returns_a_list_of_neos
+  #   results = NearEarthObjects.new('2019-03-30')
+  #   assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
+  # end
+
+  def test_it_exists
+    assert_instance_of NearEarthObjects, @results
+  end
+
+  def test_it_can_get_largest_asteroid_diameter
+    assert_equal 10233, @results.largest_astroid_diameter
   end
 end
